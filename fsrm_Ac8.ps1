5e9951304cb9889484fc2c2de477dc9e680e5eb0
UriasP17 <uriasdaddy02@gmail.com>
2026-03-23 00:10:38 -0700

    Update fsrm_Ac8.ps1

--- fsrm_Ac8.ps1
+++ fsrm_Ac8.ps1
@@ -1,10 +1,9 @@
 Write-Host "Instalando FSRM..." -ForegroundColor Cyan
 Install-WindowsFeature -Name FS-Resource-Manager -IncludeManagementTools
 
-
 Import-Module FileServerResourceManager
 
-Write-Host "Creando carpetas físicas en C:\Compartido..." -ForegroundColor Cyan
+Write-Host "Creando carpetas fisicas en C:\Compartido..." -ForegroundColor Cyan
 $rutaCuates = "C:\Compartido\Cuates"
 $rutaNoCuates = "C:\Compartido\NoCuates"
 
@@ -13,20 +12,30 @@ if (-not (Test-Path $rutaNoCuates)) { New-Item -ItemType Directory -Path $rutaNo
 
 Write-Host "Aplicando Cuotas de Disco Estrictas..." -ForegroundColor Yellow
 
-New-FsrmQuota -Path $rutaCuates -Size 10MB -Template "10 MB Limit" -Description "Cuota estricta Cuates" -ErrorAction SilentlyContinue
+# Limpiar cuotas previas si el script se corre mas de una vez
+Remove-FsrmQuota -Path $rutaCuates -ErrorAction SilentlyContinue
+Remove-FsrmQuota -Path $rutaNoCuates -ErrorAction SilentlyContinue
 
-New-FsrmQuota -Path $rutaNoCuates -Size 5MB -Template "10 MB Limit" -Description "Cuota estricta No Cuates" -ErrorAction SilentlyContinue
-Set-FsrmQuota -Path $rutaNoCuates -Size 5MB
+
+New-FsrmQuota -Path $rutaCuates -Description "Cuota estricta Cuates" -Size 10MB
+New-FsrmQuota -Path $rutaNoCuates -Description "Cuota estricta No Cuates" -Size 5MB
 
 Write-Host "Configurando el Bloqueo de Archivos (File Screen)..." -ForegroundColor Yellow
 
-New-FsrmFileGroup -Name "Prohibidos Practica" -IncludePattern @("*.mp3", "*.mp4", "*.exe", "*.msi") -ErrorAction SilentlyContinue
+
+try { 
+    New-FsrmFileGroup -Name "Prohibidos Practica" -IncludePattern @("*.mp3", "*.mp4", "*.exe", "*.msi") -ErrorAction Stop 
+} catch {}
 
 
-$EventoFSRM = New-FsrmAction -Type Event -EventType Warning -Body "BLOQUEO FSRM: El usuario [Source Io Owner] intentó guardar el archivo prohibido [Source File Path] en el servidor."
+Remove-FsrmFileScreen -Path $rutaCuates -ErrorAction SilentlyContinue
+Remove-FsrmFileScreen -Path $rutaNoCuates -ErrorAction SilentlyContinue
 
 
-New-FsrmFileScreen -Path $rutaCuates -Active $true -FileGroup "Prohibidos Practica" -Notification $EventoFSRM -ErrorAction SilentlyContinue
-New-FsrmFileScreen -Path $rutaNoCuates -Active $true -FileGroup "Prohibidos Practica" -Notification $EventoFSRM -ErrorAction SilentlyContinue
+$EventoFSRM = New-FsrmAction -Type Event -EventType Warning -Body "BLOQUEO FSRM: El usuario [Source Io Owner] intento guardar el archivo prohibido [Source File Path] en el servidor."
+
+
+New-FsrmFileScreen -Path $rutaCuates -Active -IncludeGroup "Prohibidos Practica" -Notification $EventoFSRM
+New-FsrmFileScreen -Path $rutaNoCuates -Active -IncludeGroup "Prohibidos Practica" -Notification $EventoFSRM
 
 Write-Host "¡FSRM configurado al 100%!" -ForegroundColor Green
